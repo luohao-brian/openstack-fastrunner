fd6b5e1c618138d45da396dc946170df6c930a35
XianChaobo <xianchaobo@gmail.com>
2017-07-03 23:00:54 +0800

    add fastrunner API
    Signed-off-by: XianChaobo <xianchaobo@gmail.com>

---
+++ fastrunner/api/openstack/compute/servers.py
@@ -0,0 +1,127 @@
+# Copyright 2010 OpenStack Foundation
+# Copyright 2011 Piston Cloud Computing, Inc
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import base64
+import re
+
+from oslo_config import cfg
+from oslo_log import log as logging
+import oslo_messaging as messaging
+from oslo_utils import strutils
+from oslo_utils import timeutils
+from oslo_utils import uuidutils
+import six
+import stevedore
+import webob
+from webob import exc
+
+from fastrunner.api.openstack import extensions
+from fastrunner.api.openstack import wsgi
+from fastrunner.api import validation
+from fastrunner import exception
+from fastrunner.i18n import _
+from fastrunner.i18n import _LW
+from fastrunner import utils
+
+ALIAS = 'servers'
+
+CONF = cfg.CONF
+CONF.import_opt('extensions_blacklist', 'fastrunner.api.openstack',
+                group='osapi_v21')
+CONF.import_opt('extensions_whitelist', 'fastrunner.api.openstack',
+                group='osapi_v21')
+
+LOG = logging.getLogger(__name__)
+authorize = extensions.os_compute_authorizer(ALIAS)
+
+
+class ServersController(wsgi.Controller):
+    """The Server API base controller class for the OpenStack API."""
+
+
+    @staticmethod
+    def _add_location(robj):
+        # Just in case...
+        if 'server' not in robj.obj:
+            return robj
+
+        link = [l for l in robj.obj['server']['links'] if l['rel'] == 'self']
+        if link:
+            robj['Location'] = utils.utf8(link[0]['href'])
+
+        # Convenience return
+        return robj
+
+    def __init__(self, **kwargs):
+        self.extension_info = kwargs.pop('extension_info')
+        super(ServersController, self).__init__(**kwargs)
+
+    @extensions.expected_errors((400, 403))
+    def index(self, req):
+        """Returns a list of server names and ids for a given user."""
+        LOG.info("TODO index")
+
+    @extensions.expected_errors((400, 403))
+    def detail(self, req):
+        """Returns a list of server details for a given user."""
+        LOG.info("TODO detail")
+
+
+    @extensions.expected_errors(404)
+    def show(self, req, id):
+        """Returns server details by server id."""
+        LOG.info("TODO show")
+
+
+
+def remove_invalid_options(context, search_options, allowed_search_options):
+    """Remove search options that are not valid for non-admin API/context."""
+    if context.is_admin:
+        # Only remove parameters for sorting and pagination
+        for key in ('sort_key', 'sort_dir', 'limit', 'marker'):
+            search_options.pop(key, None)
+        return
+    # Otherwise, strip out all unknown options
+    unknown_options = [opt for opt in search_options
+                        if opt not in allowed_search_options]
+    if unknown_options:
+        LOG.debug("Removing options '%s' from query",
+                  ", ".join(unknown_options))
+        for opt in unknown_options:
+            search_options.pop(opt, None)
+
+
+class Servers(extensions.V21APIExtensionBase):
+    """Servers."""
+
+    name = "Servers"
+    alias = ALIAS
+    version = 1
+
+    def get_resources(self):
+        member_actions = {'action': 'POST'}
+        collection_actions = {'detail': 'GET'}
+        resources = [
+            extensions.ResourceExtension(
+                ALIAS,
+                ServersController(extension_info=self.extension_info),
+                member_name='server', collection_actions=collection_actions,
+                member_actions=member_actions)]
+
+        return resources
+
+    def get_controller_extensions(self):
+        return []
---
+++ fastrunner/config.py
@@ -0,0 +1,61 @@
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# Copyright 2012 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_cache import core as cache
+from oslo_config import cfg
+from oslo_db import options
+from oslo_log import log
+
+from fastrunner.common import config
+from fastrunner import debugger
+from fastrunner import paths
+from fastrunner import version
+
+CONF = cfg.CONF
+
+_DEFAULT_SQL_CONNECTION = 'sqlite:///' + paths.state_path_def('fastrunner.sqlite')
+
+# NOTE(mikal): suds is used by the vmware driver, removing this will
+# cause many extraneous log lines for their tempest runs. Refer to
+# https://review.openstack.org/#/c/219225/ for details.
+_DEFAULT_LOG_LEVELS = ['amqp=WARN', 'amqplib=WARN', 'boto=WARN',
+                       'qpid=WARN', 'sqlalchemy=WARN', 'suds=INFO',
+                       'oslo_messaging=INFO', 'iso8601=WARN',
+                       'requests.packages.urllib3.connectionpool=WARN',
+                       'urllib3.connectionpool=WARN', 'websocket=WARN',
+                       'keystonemiddleware=WARN', 'routes.middleware=WARN',
+                       'stevedore=WARN', 'glanceclient=WARN']
+
+_DEFAULT_LOGGING_CONTEXT_FORMAT = ('%(asctime)s.%(msecs)03d %(process)d '
+                                   '%(levelname)s %(name)s [%(request_id)s '
+                                   '%(user_identity)s] %(instance)s'
+                                   '%(message)s')
+
+
+def parse_args(argv, default_config_files=None, configure_db=True,
+               init_rpc=True):
+    log.set_defaults(_DEFAULT_LOGGING_CONTEXT_FORMAT, _DEFAULT_LOG_LEVELS)
+    log.register_options(CONF)
+    options.set_defaults(CONF, connection=_DEFAULT_SQL_CONNECTION,
+                         sqlite_db='fastrunner.sqlite')
+    cache.configure(CONF)
+    debugger.register_cli_opts()
+    config.set_middleware_defaults()
+
+    CONF(argv[1:],
+         project='fastrunner',
+         default_config_files=default_config_files)
